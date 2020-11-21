c772d4f0c1883a29c4eca6ef05033ace3785cf3a
pranas <pranas@orangecap.net>
2020-11-21 11:25:41 +1100

    Create demo project for remote JMX access of Spring boot 2.4 application

---
+++ src/main/java/com/example/restservice/Greeting.java
@@ -0,0 +1,14 @@
+package com.example.restservice;
+
+public class Greeting {
+
+  private final String content;
+
+  public Greeting(String content) {
+    this.content = content;
+  }
+
+  public String getContent() {
+    return content;
+  }
+}
